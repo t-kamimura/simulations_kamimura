1736281fe77147b1b59b84a3220260a252b69366
t-kamimura <tenjinsan@gmail.com>
2020-06-19 12:04:31 +0900

    足先位置は接地した瞬間の値を定数として与えることにした

--- Program_TwoLegRigidBody/symbolic/symbolic_doublestance.m
+++ Program_TwoLegRigidBody/symbolic/symbolic_doublestance.m
@@ -9,11 +9,10 @@ close all
 %% definition
 
 % parameters
-syms m J kh kf x_ini gamma_h_td gamma_f_td  % x_ini : 接地した瞬間の質量中心位置の状態量
+syms m J kh kf xf_toe xh_toe gamma_h_td gamma_f_td% x*_toe :足先位置
 syms l l0
 syms g
-param = [m J kh kf x_ini gamma_h_td  gamma_f_td l l0 g]
-
+param = [m J kh kf x_ini gamma_h_td gamma_f_td l l0 g]
 
 % state variables
 syms x y theta
@@ -26,45 +25,39 @@ syms T1 T2 U1 U2 U3
 syms L
 
 %それ以外のパラメータ定義
-Xh = x - l*cos(theta);
-Yh = y- l*sin(theta);
-tilde_Xh = x_ini - l*cos(theta)+ l0*sin(gamma_h_td);
-delta_xh = tilde_Xh - Xh;
-%gamma_h_td = arctan(delta_xh/Yh);
+Xh = x - l * cos(theta);    % hip position
+Yh = y - l * sin(theta);    % hip position
+delta_xh = xh_toe - Xh;
 lb = sqrt(Yh^2 + delta_xh^2);
 
-Xf = x + l*cos(theta);
-Yf = y + l*sin(theta);
-tilde_Xf = x_ini + l*cos(theta) + l0*sin(gamma_f_td);
-delta_xf = tilde_Xf - Xf;
+Xf = x + l * cos(theta);
+Yf = y + l * sin(theta);
+delta_xf = xf_toe - Xf;
 %gamma_f_td = arctan(delta_xf/Yf);
 lf = sqrt(Yf^2 + delta_xf^2);
 
-
-
-
 % Energy
-T1 = 0.5*m*(dx^2 + dy^2); % 並進の運動エネルギー
-T2 = 0.5*J*m*l^2*dtheta^2; % 回転の運動エネルギー
-U1 = m*g*y; % 重力のポテンシャルエネルギー
-U2 = 0.5*kh*(l0- lb)^2; % 後足バネのポテンシャルエネルギー
-U3 = 0.5*kf*(l0- lf)^2; % 前足バネのポテンシャルエネルギー
- 
+T1 = 0.5 * m * (dx^2 + dy^2); % 並進の運動エネルギー
+T2 = 0.5 * J * m * l^2 * dtheta^2; % 回転の運動エネルギー
+U1 = m * g * y; % 重力のポテンシャルエネルギー
+U2 = 0.5 * kh * (l0 - lb)^2; % 後足バネのポテンシャルエネルギー
+U3 = 0.5 * kf * (l0 - lf)^2; % 前足バネのポテンシャルエネルギー
+
 L = simplify(T1 + T2 - U1 - U2 - U3);
 E = simplify(T1 + T2 + U1 + U2 + U3);
 
 % Differentials
-dLddq = jacobian(L,dq);
-d_dLddq_dt = jacobian(dLddq,q)*dq.';
-dLdq = jacobian(L,q);
+dLddq = jacobian(L, dq);
+d_dLddq_dt = jacobian(dLddq, q) * dq.';
+dLdq = jacobian(L, q);
 
-M = jacobian(dLddq,dq);   % Inertia matrix
+M = jacobian(dLddq, dq); % Inertia matrix
 M = simplify(M);
 
 f_cg = d_dLddq_dt - dLdq.'; % Corioris & gravitational force
 f_cg = simplify(f_cg);
 
 % save as functions
-matlabFunction(M,'file','myMassMatrix_Doublestance','vars',{q, param});
-matlabFunction(f_cg,'file','myF_CoriGrav_Doublestance','vars',{q, dq, param});
-matlabFunction(E,'file','myTotalEnergy_Doublestance','vars',{q, dq, param});
+matlabFunction(M, 'file', 'myMassMatrix_Doublestance', 'vars', {q, param});
+matlabFunction(f_cg, 'file', 'myF_CoriGrav_Doublestance', 'vars', {q, dq, param});
+matlabFunction(E, 'file', 'myTotalEnergy_Doublestance', 'vars', {q, dq, param});
--- Program_TwoLegRigidBody/symbolic/symbolic_hind.m
+++ Program_TwoLegRigidBody/symbolic/symbolic_hind.m
@@ -9,11 +9,10 @@ close all
 %% definition
 
 % parameters
-syms m J kh kf x_ini gamma_h_td  % x_ini : 接地した瞬間の質量中心位置の状態量
+syms m J kh kf xh_toe gamma_h_td % xh_toe :足先位置 
 syms l l0
 syms g
 param = [m J kh kf x_ini gamma_h_td l l0 g]
-
 
 % state variables
 syms x y theta
@@ -26,38 +25,33 @@ syms T1 T2 U1 U2 U3
 syms L
 
 %それ以外のパラメータ定義
-Xh = x - l*cos(theta);
-Yh = y- l*sin(theta);
-tilde_Xh = x_ini - l*cos(theta)+ l0*sin(gamma_h_td);
-delta_xh = tilde_Xh - Xh;
-%gamma_h_td = arctan(delta_xh/Yh);
+Xh = x - l * cos(theta);    % hip position
+Yh = y - l * sin(theta);    % hip position
+delta_xh = xh_toe - Xh;
 lb = sqrt(Yh^2 + delta_xh^2);
 
-
-
-
 % Energy
-T1 = 0.5*m*(dx^2 + dy^2); % 並進の運動エネルギー
-T2 = 0.5*J*m*l^2*dtheta^2; % 回転の運動エネルギー
-U1 = m*g*y; % 重力のポテンシャルエネルギー
-U2 = 0.5*kh*(l0- lb)^2; % 後足バネのポテンシャルエネルギー
+T1 = 0.5 * m * (dx^2 + dy^2); % 並進の運動エネルギー
+T2 = 0.5 * J * m * l^2 * dtheta^2; % 回転の運動エネルギー
+U1 = m * g * y; % 重力のポテンシャルエネルギー
+U2 = 0.5 * kh * (l0 - lb)^2; % 後足バネのポテンシャルエネルギー
 U3 = 0; % 前足バネのポテンシャルエネルギー
- 
+
 L = simplify(T1 + T2 - U1 - U2 - U3);
 E = simplify(T1 + T2 + U1 + U2 + U3);
 
 % Differentials
-dLddq = jacobian(L,dq);
-d_dLddq_dt = jacobian(dLddq,q)*dq.';
-dLdq = jacobian(L,q);
+dLddq = jacobian(L, dq);
+d_dLddq_dt = jacobian(dLddq, q) * dq.';
+dLdq = jacobian(L, q);
 
-M = jacobian(dLddq,dq);   % Inertia matrix
+M = jacobian(dLddq, dq); % Inertia matrix
 M = simplify(M);
 
 f_cg = d_dLddq_dt - dLdq.'; % Corioris & gravitational force
 f_cg = simplify(f_cg);
 
 % save as functions
-matlabFunction(M,'file','myMassMatrix_Hind','vars',{q, param});
-matlabFunction(f_cg,'file','myF_CoriGrav_Hind','vars',{q, dq, param});
-matlabFunction(E,'file','myTotalEnergy_Hind','vars',{q, dq, param});
+matlabFunction(M, 'file', 'myMassMatrix_Hind', 'vars', {q, param});
+matlabFunction(f_cg, 'file', 'myF_CoriGrav_Hind', 'vars', {q, dq, param});
+matlabFunction(E, 'file', 'myTotalEnergy_Hind', 'vars', {q, dq, param});
