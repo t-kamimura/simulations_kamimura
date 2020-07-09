af20c4fd0f5584c22c38ebab1d678778fa95d180
t-kamimura <tenjinsan@gmail.com>
2020-07-09 18:08:41 +0900

    symbolic_flightの文字の定義を修正．今後これに合わせてほしい

--- Program_TwoLegFlexibleBody/symbolic/symbolic_flight.m
+++ Program_TwoLegFlexibleBody/symbolic/symbolic_flight.m
@@ -9,10 +9,11 @@ close all
 %% definition
 
 % parameters
-syms  m J kt x_h y_h x_f y_f 
+syms m J kt xh yh xf xf
 syms l l0
 syms g
-param = [ m J kt x_h y_h x_f y_f l l0 g]
+% param = [ m J kt xh yh xf xf l l0 g]
+param = [m J kt l l0 g]% 定数のみ
 
 % state variables
 syms x y theta phi
@@ -25,22 +26,22 @@ syms T1 T2 U1 U2 U3 U4
 syms L
 
 %それ以外のパラメータ
-x_h = x - l * cos(phi) * cos(theta);
-y_h = y - l * cos(phi) * sin(theta);
-x_f = x + l * cos(phi) * cos(theta);
-y_f = y + l * cos(phi) * sin(theta);
-dx_h = jacobian(x_h,q) * dq.';
-dy_h = jacobian(y_h,q) * dq.';
-dx_f = jacobian(x_f,q) * dq.';
-dy_f = jacobian(x_f,q) * dq.';
+xh = x - l * cos(phi) * cos(theta);
+yh = y - l * cos(phi) * sin(theta);
+xf = x + l * cos(phi) * cos(theta);
+xf = y + l * cos(phi) * sin(theta);
+dxh = jacobian(xh, q) * dq.';
+dyh = jacobian(yh, q) * dq.';
+dxf = jacobian(xf, q) * dq.';
+dxf = jacobian(xf, q) * dq.';
 
 % Energy
-T1 = 0.5 * m * (dx_h^2 + dy_h^2) + 0.5 * m * (dx_f^2 +dy_f^2 ); % 並進の運動エネルギー
+T1 = 0.5 * m * (dxh^2 + dyh^2) + 0.5 * m * (dxf^2 + dxf^2); % 並進の運動エネルギー
 T2 = J * (dtheta^2 + dphi^2); % TODO: 回転の運動エネルギー
 U1 = 2 * m * g * y; % 重力のポテンシャルエネルギー
 U2 = 0; % 後足バネのポテンシャルエネルギー
 U3 = 0; % 前足バネのポテンシャルエネルギー
-U4 = 0.5 * kt * (2*phi)^2;  % 体幹バネのポテンシャルエネルギー
+U4 = 0.5 * kt * (2 * phi)^2; % 体幹バネのポテンシャルエネルギー
 
 L = simplify(T1 + T2 - U1 - U2 - U3 - U4);
 E = simplify(T1 + T2 + U1 + U2 + U3 + U4);
@@ -57,6 +58,6 @@ f_cg = d_dLddq_dt - dLdq.'; % Corioris & gravitational force
 f_cg = simplify(f_cg);
 
 % save as functions
-matlabFunction(M, 'file', 'myMassMatrix_Flight', 'vars', {q, param});
+matlabFunction(M, 'file', 'myMassMatrixflight', 'vars', {q, param});
 matlabFunction(f_cg, 'file', 'myF_CoriGrav_Flight', 'vars', {q, dq, param});
-matlabFunction(E, 'file', 'myTotalEnergy_Flight', 'vars', {q, dq, param});
+matlabFunction(E, 'file', 'myTotalEnergxflight', 'vars', {q, dq, param});
