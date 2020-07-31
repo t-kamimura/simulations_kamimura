6a7588e87a49472131bb310133e88d49d291f8ad
sato kaho <clf13078@ict.nitech.ac.jp>
2020-07-31 18:00:34 +0900

    動作可能になった

--- Program_TwoLegFlexibleBody/main_test.m
+++ Program_TwoLegFlexibleBody/main_test.m
@@ -24,18 +24,18 @@ addpath(pwd, 'fig')
 
 model = Twoleg;
 
-x_ini = 0.0;
+x_ini = f;
 y_ini = 1.0;
 theta_ini = 0;
-phi_ini = deg2rad(5);
-dx_ini = 0;
+phi_ini = deg2rad(10);
+dx_ini = 2.5;
 dy_ini = 0;
 dtheta_ini = deg2rad(0);
 dphi_ini = 0;
 % gb_ini = 0*pi / 8;
 % gf_ini = 0*pi / 8;
-gamma_h_td_ini = deg2rad(0);
-gamma_f_td_ini = deg2rad(0);
+gamma_h_td_ini = deg2rad(14);
+gamma_f_td_ini = deg2rad(14);
 
 q_ini = [x_ini y_ini theta_ini phi_ini dx_ini dy_ini dtheta_ini dphi_ini];
 u_ini = [gamma_h_td_ini gamma_f_td_ini];
@@ -44,4 +44,4 @@ model.init
 model.bound(q_ini, u_ini)
 
 model.plot(saveflag)
-model.anime(0.05, false);
+model.anime(0.1, false);
